e99eab4d0dc797b49f1732688e8cd62e2d8fd7cb
Felipe Martins <felipemartins@MacBook-Pro-de-Felipe.local>
2019-10-31 17:43:21 -0300

    remove features which sigma calculation equals to zero

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ shape_view = data.values[:, 0:9]
 # rgb_view([2100]points (n), [10]features (p))
 rgb_view = data.values[:, 9:19]
 
+# Remove sigma = 0 features
+shape_view = shape_view[:,[0,1,3,5,6,7,8]]
+
 # Normalize data
 rgb_view = minmax_scale(rgb_view, feature_range=(0, 1), axis=0)
 shape_view = minmax_scale(shape_view, feature_range=(0, 1), axis=0)

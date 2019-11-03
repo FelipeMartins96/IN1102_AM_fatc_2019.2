bb5702346a61e2193569b668b02ba15e3a650569
Felipe Martins <felipemartins@MacBook-Pro-de-Felipe.local>
2019-11-03 20:39:55 -0300

    change groundthruth source

--- labels.py
+++ labels.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np 
 
-data = pd.read_csv('data/segmentationnh.test')
+data = pd.read_csv('data/seg.test')
 label = {'FOLIAGE': 0, 'PATH': 1, 'CEMENT': 2, 'GRASS': 3, 'WINDOW': 4, 'SKY': 5, 'BRICKFACE': 6}
 data.index = [label[item] for item in data.index] 
 index = data.index

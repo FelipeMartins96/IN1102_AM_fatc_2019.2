5319b0918103fbf6d94958dd409b186d7300fbf8
Felipe Martins <felipemartins@MacBook-Pro-de-Felipe.local>
2019-11-03 20:39:13 -0300

    changed defuzzyfy to output label vector

--- defuzzyfy.py
+++ defuzzyfy.py
@@ -7,12 +7,12 @@ if len(sys.argv) is not 2:
 
 df = pd.read_csv(sys.argv[1] + "/best_u.csv", header=None)
 
-# crisp = df.idxmax().values
-# np.savetxt(sys.argv[1] + "/crisp.csv", crisp, delimiter=",")
+crisp = df.idxmax().values
+np.savetxt(sys.argv[1] + "/crisp.csv", crisp, delimiter=",")
 
-new_df = pd.DataFrame()
-for feature in df.columns:
-    max = df[feature].max()
-    new_df[feature] = df[feature].apply(lambda x: 1 if x == max else 0)
-new_df.to_csv(sys.argv[1] + '/crisp.csv', index=False, header=False)
+# new_df = pd.DataFrame()
+# for feature in df.columns:
+#     max = df[feature].max()
+#     new_df[feature] = df[feature].apply(lambda x: 1 if x == max else 0)
+# new_df.to_csv(sys.argv[1] + '/crisp.csv', index=False, header=False)
 

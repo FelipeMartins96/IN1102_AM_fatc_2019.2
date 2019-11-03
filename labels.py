b02f3395be455e0c07c4ef2f779644a0a00e3546
Felipe Martins <felipemartins@MacBook-Pro-de-Felipe.local>
2019-11-03 20:38:21 -0300

    get groundtruth label from data code

---
+++ labels.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np 
+
+data = pd.read_csv('data/segmentationnh.test')
+label = {'FOLIAGE': 0, 'PATH': 1, 'CEMENT': 2, 'GRASS': 3, 'WINDOW': 4, 'SKY': 5, 'BRICKFACE': 6}
+data.index = [label[item] for item in data.index] 
+index = data.index
+
+np.savetxt("data/test_gt.csv", index, delimiter=",")
